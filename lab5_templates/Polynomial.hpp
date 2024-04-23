cb246f02b30183b7cd8a3e7d4a847553f5c94928
Michal Brzezinski <michal.brzezinski.off@gmail.com>
2024-04-23 23:19:30 +0200

    Final version, operators out of class definition to service different template parameters, which is not possible inside class. Also it is not possible to work on template class'es methods in other source file (not in header file)

--- lab5_templates/Polynomial.hpp
+++ lab5_templates/Polynomial.hpp
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <vector>
+#include <cmath>
 
 
 using namespace std;
@@ -19,20 +20,91 @@ public:
     const std::vector<double>& getCoefficients() const {
         return coefficients;
     }
+  
+
+    double evaluate(double x) const {
+        double result = 0;
+        for (int i = 0; i <= Degree; ++i) {
+            result += coefficients[i] * pow(x, Degree-i);  
+            //Musze takie zrobiæ ¿eby obliczenie dzia³a³o dobrze a nie od koñca
+        }
+        return result;
+    }
+
 };
+
 
 template <int Degree>
 void printPolynomial(const Polynomial<Degree>& poly) {
     std::cout << "Wielomian jest postaci: ";
     int i = Degree;
     for (const auto& elem : poly.getCoefficients()) { // U¿ywamy nowej funkcji
+        
         if (i != 0) {
-            std::cout << elem << "x^" << i << " + ";
+            if (elem !=0)
+                std::cout << elem << "x^" << i << " + ";
             i--;
         }
-        else std::cout << elem;
+        else
+        {
+            if (elem!=0)
+            std::cout << elem;
+        }
     }
     std::cout << std::endl;
+}
+
+
+
+template <int Degree1, int Degree2>
+auto operator+(const Polynomial<Degree1>& p1, const Polynomial<Degree2>& p2) {
+    constexpr int maxDegree = Degree1 > Degree2 ? Degree1 : Degree2;
+    std::vector<double> coefficients(maxDegree + 1, 0.0);
+
+    const auto& coeffs1 = p1.getCoefficients();
+    const auto& coeffs2 = p2.getCoefficients();
+
+    for (int i = 0; i <= Degree1; ++i) {
+        coefficients[coefficients.size()-coeffs1.size()+i] += coeffs1[i];
+    }
+
+    for (int i = 0; i <= Degree2; ++i) {
+        coefficients[coefficients.size() - coeffs2.size() + i] += coeffs2[i];
+    }
+
+    return Polynomial<maxDegree>(coefficients);
+}
+
+
+template <int Degree1, int Degree2>
+auto operator-(const Polynomial<Degree1>& p1, const Polynomial<Degree2>& p2) {
+    constexpr int maxDegree = Degree1 > Degree2 ? Degree1 : Degree2;
+    std::vector<double> coefficients(maxDegree + 1, 0.0);
+
+    const auto& coeffs1 = p1.getCoefficients();
+    const auto& coeffs2 = p2.getCoefficients();
+
+    for (int i = 0; i <= Degree1; ++i) {
+        coefficients[coefficients.size() - coeffs1.size() + i] += coeffs1[i];
+    }
+
+    for (int i = 0; i <= Degree2; ++i) {
+        coefficients[coefficients.size() - coeffs2.size() + i] -= coeffs2[i];
+    }
+
+    return Polynomial<maxDegree>(coefficients);
+}
+
+
+template <int Degree>
+Polynomial<Degree> operator*(const Polynomial<Degree>& p, double scalar) {
+    std::vector<double> coefficients = p.getCoefficients();
+
+    for (int i = 0; i <= Degree; ++i) {
+        coefficients[i] *= scalar;
+    }
+
+    return Polynomial<Degree>(coefficients);
 }
 
 #endif
--- lab5_templates/main.cpp
+++ lab5_templates/main.cpp
@@ -10,33 +10,30 @@ int main() {
     // Utwórz kilka wielomianów i przetestuj operacje na nich
     Polynomial<3> poly1({ 1, 2, 3, 4 });
     printPolynomial(poly1);// x^3 + 2x^2 + 3x + 4
-    //Polynomial<2> poly2({ -1, 0, 1 }); // -x^2 + 1
-    /*
+    Polynomial<2> poly2({ -1, 0, 1}); // -x^2 + 1
+    printPolynomial(poly2);
+    
     // Dodaj dwa wielomiany
     auto sum = poly1 + poly2;
-    std::cout << "Suma wielomianów: ";
+    std::cout << "Suma wielomianow: ";
     printPolynomial(sum);
-
+   
+    
+    
     // Odejmij jeden wielomian od drugiego
     auto diff = poly1 - poly2;
-    std::cout << "Ró¿nica wielomianów: ";
+    std::cout << "Roznica wielomianow: ";
     printPolynomial(diff);
 
     // Pomnó¿ wielomian przez sta³¹
     auto scaled = poly1 * 2;
     std::cout << "Wielomian pomno¿ony przez 2: ";
     printPolynomial(scaled);
-
+    
     // Oblicz wartoœæ wielomianu dla x = 2
     double x = 2;
-    std::cout << "Wartoœæ wielomianu dla x = " << x << ": " << poly1.calculate(x) << std::endl;
-    */
+    std::cout << "Wartosc wielomianu poly1 dla x = " << x << ": " << poly1.evaluate(x)<< std::endl;
+    
 
     return 0;
-}
-
-int print()
-{
-    std::cout << "Pies";
-    return 2;
 }
